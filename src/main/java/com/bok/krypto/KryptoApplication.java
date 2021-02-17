374f6c96e3beba871592fa1775835e5352903c41
cfaraone <cfaraone@soldo.com>
2021-02-17 11:58:04 +0100

    initialized project krypto

---
+++ src/main/java/com/bok/krypto/KryptoApplication.java
@@ -0,0 +1,13 @@
+package com.bok.krypto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KryptoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KryptoApplication.class, args);
+    }
+
+}
