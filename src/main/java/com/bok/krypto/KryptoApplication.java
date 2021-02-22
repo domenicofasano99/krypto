673562a713ec5143ebbe8361df58ae36fce838eb
cfaraone <cfaraone@soldo.com>
2021-02-22 14:05:25 +0100

    minor modifications

--- src/main/java/com/bok/krypto/KryptoApplication.java
+++ src/main/java/com/bok/krypto/KryptoApplication.java
@@ -3,8 +3,10 @@ package com.bok.krypto;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class KryptoApplication {
 
     public static void main(String[] args) {
