3e29a9e1c6aa6b65c389336c0d9f388e8f4dd5d8
cfaraone <cfaraone@soldo.com>
2021-03-14 17:48:24 +0100

    fixed broken test

--- src/test/java/com/bok/krypto/KryptoApplicationTests.java
+++ src/test/java/com/bok/krypto/KryptoApplicationTests.java
@@ -1,14 +1,10 @@
 package com.bok.krypto;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class KryptoApplicationTests {
-
-    @Autowired
-    TransactionProducer transactionProducer;
 
     @Test
     void contextLoads() {
