a21e6278602755bc428ad425df836099062a43a0
cfaraone <cfaraone@soldo.com>
2021-03-24 23:19:20 +0100

    test of a modified zuul version

--- src/main/java/com/bok/krypto/controller/Test.java
+++ src/main/java/com/bok/krypto/controller/Test.java
@@ -1,13 +1,14 @@
 package com.bok.krypto.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Test {
 
     @GetMapping("/hello")
-    public String hello() {
-        return "hello from the other side";
+    public String hello(@RequestParam("userId") Long userId) {
+        return userId + "hello from the other side";
     }
 }
