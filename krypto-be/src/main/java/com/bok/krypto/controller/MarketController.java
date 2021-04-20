badccb21fe05398730ff116ffe2cc66c4c4c70bc
cfaraone <cfaraone@soldo.com>
2021-04-20 20:36:49 +0200

    fixed an error on some controllers

--- krypto-be/src/main/java/com/bok/krypto/controller/MarketController.java
+++ krypto-be/src/main/java/com/bok/krypto/controller/MarketController.java
@@ -13,6 +13,13 @@ import com.bok.integration.krypto.dto.SellRequestDTO;
 import com.bok.integration.krypto.dto.TransactionDTO;
 import com.bok.krypto.service.interfaces.MarketService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/market")
--- krypto-be/src/main/java/com/bok/krypto/controller/TransferController.java
+++ krypto-be/src/main/java/com/bok/krypto/controller/TransferController.java
@@ -5,6 +5,13 @@ import com.bok.integration.krypto.dto.TransferRequestDTO;
 import com.bok.integration.krypto.dto.TransferResponseDTO;
 import com.bok.krypto.service.interfaces.TransferService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/transfer")
