b58cf5456696797fc0d402b40e346b44e04c111d
cfaraone <christiangennaro.faraone@mail.polimi.it>
2021-08-22 17:56:28 +0200

    made transactional a method (just to test)

--- krypto-be/src/main/java/com/bok/krypto/helper/HistoricalDataHelper.java
+++ krypto-be/src/main/java/com/bok/krypto/helper/HistoricalDataHelper.java
@@ -4,12 +4,12 @@ import com.bok.krypto.exception.KryptoNotFoundException;
 import com.bok.krypto.integration.internal.dto.HistoricalDataDTO;
 import com.bok.krypto.integration.internal.dto.RecordDTO;
 import com.bok.krypto.model.HistoricalData;
-import com.bok.krypto.model.Krypto;
 import com.bok.krypto.repository.HistoricalDataRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.transaction.Transactional;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
@@ -25,7 +25,7 @@ public class HistoricalDataHelper {
     @Autowired
     KryptoHelper kryptoHelper;
 
-
+    @Transactional
     public HistoricalDataDTO getKryptoHistoricalData(String symbol, Instant from, Instant until) {
         if (!kryptoHelper.existsBySymbol(symbol)) {
             throw new KryptoNotFoundException("Krypto " + symbol + " does not exist");
@@ -35,11 +35,13 @@ public class HistoricalDataHelper {
         }
 
         //due to Hibernate problems the query with Instants and between is not working, the support for this kind of complex queries should be added in following versions
-        List<HistoricalData> data = historicalDataRepository.findByKrypto_Symbol(symbol).stream()
+        List<HistoricalData> data = historicalDataRepository.findByKrypto_Symbol(symbol);
+        log.info("{} has a {} records in total, filtering...", symbol, data.size());
+        data = data.stream()
                 .filter(h -> h.getTimestamp().isAfter(from) && h.getTimestamp().isBefore(until))
                 .collect(Collectors.toList());
 
-        log.info("found {} records for Krypto {} between dates {} and {}", data.size(), symbol, from, until);
+        log.info("found {} records for {} between from {} until {}", data.size(), symbol, from, until);
         HistoricalDataDTO dto = new HistoricalDataDTO();
         dto.symbol = symbol;
         dto.history = new ArrayList<>();
